78f454fc5f768a2169545b58fd9a5698262ec9a3
Moritz Sallermann <moritzsallermann@gmail.com>
2025-02-21 14:25:31 +0000

    lennard_jones: remove double counting of virial

--- src/lennard_jones.cpp
+++ src/lennard_jones.cpp
@@ -61,39 +61,34 @@ double LennardJones::energy_and_forces( const Eigen::Ref<Vectorfield> positions,
     check_buffers( n_atoms );
 
     // zero out the forces and energy buffer
-    Backend::for_each(
-        n_atoms,
-        [&]( int i )
-        {
-            forces.row( i )        = Vector3::Zero();
-            energy_buffer_atoms[i] = 0.0;
-            virial_buffer_atoms[i] = 0.0;
-        } );
+    Backend::for_each( n_atoms, [&]( int i ) {
+        forces.row( i )        = Vector3::Zero();
+        energy_buffer_atoms[i] = 0.0;
+        virial_buffer_atoms[i] = 0.0;
+    } );
 
-    iterate_neighbours(
-        rc, box, positions, neighbour_indices,
-        [&]( int n, int m, const Vector3 & r )
-        {
-            const double R         = r.norm();
-            const double R2        = R * R;
-            const double sigma_R   = sigma / R;
-            const double sigma_R_2 = sigma_R * sigma_R;
-            const double sigma_R_4 = sigma_R_2 * sigma_R_2;
-            const double sigma_R_6 = sigma_R_4 * sigma_R_2;
+    iterate_neighbours( rc, box, positions, neighbour_indices, [&]( int n, int m, const Vector3 & r ) {
+        const double R         = r.norm();
+        const double R2        = R * R;
+        const double sigma_R   = sigma / R;
+        const double sigma_R_2 = sigma_R * sigma_R;
+        const double sigma_R_4 = sigma_R_2 * sigma_R_2;
+        const double sigma_R_6 = sigma_R_4 * sigma_R_2;
 
-            // V_{ij} = 4 \epsilon * ( (\sigma/R_{ij})^12 - (\sigma/R_{ij})^6 )
-            const double Vij = 4.0 * epsilon * sigma_R_6 * ( sigma_R_6 - 1.0 );
+        // V_{ij} = 4 \epsilon * ( (\sigma/R_{ij})^12 - (\sigma/R_{ij})^6 )
+        const double Vij = 4.0 * epsilon * sigma_R_6 * ( sigma_R_6 - 1.0 );
 
-            // factor of 1/2 to account for double counting in the neighbour list
-            energy_buffer_atoms[n] += 0.5 * Vij;
+        // factor of 1/2 to account for double counting in the neighbour list
+        energy_buffer_atoms[n] += 0.5 * Vij;
 
-            const double dVij_drij = 24.0 * epsilon / R2 * sigma_R_6 * ( 2.0 * sigma_R_6 - 1.0 );
+        const double dVij_drij = 24.0 * epsilon / R2 * sigma_R_6 * ( 2.0 * sigma_R_6 - 1.0 );
 
-            const Vector3 fij = dVij_drij * r;
-            forces.row( n ) += fij;
+        const Vector3 fij = dVij_drij * r;
+        forces.row( n ) += fij;
 
-            virial_buffer_atoms[n] += fij.dot( r );
-        } );
+        // Divide by 2 to remove double counting of pairs (same reason we divide the energy by 2)
+        virial_buffer_atoms[n] += 0.5 * fij.dot( r );
+    } );
 
     virial = Backend::sum<double>( virial_buffer_atoms );
     return Backend::sum<double>( energy_buffer_atoms );
